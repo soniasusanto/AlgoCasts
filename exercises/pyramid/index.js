495b6c0a79c036a26bd1adba6ea53d7e8755f618
Sonia Susanto <soniasusanto@gmail.com>
2020-04-23 00:10:25 -0400

    completed up till section 15

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,44 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// function pyramid(n) {
+//   let mid = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (mid - row <= column && mid + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+function pyramid(n, row = 0, level = '') {
+  //if row === n, we are on the last row and should return
+  if (row === n) {
+    return;
+  }
+  //if the string 'level' has a length equal to 2*n-1 (max possible length,for a given n, its length will be 2n-1), it means it has reached the end of the string and we should console log it. Call the function again to proceed to the next row by incrementing row.
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  //if not, we are still in the midst of building the level. Calculate midpoint.
+  let mid = Math.floor((2 * n - 1) / 2);
+  let add;
+  //since we are building the string as we go along, level.length represents the column we are at. This checks both the left and right boundaries of the string.
+  if (mid - row <= level.length && mid + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  //call the function again with the updated string (appending the add variable to level)
+  pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,23 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   let vowels = 'aeiouAEIOU';
+//   let count = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (vowels.includes(str[i])) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+//regex solution
+
+function vowels(str) {
+  //g allows us to find all possible matches, so it doesn't stop at one match. the i stands for case insensitive, so it can detect both upper and lower cases
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
